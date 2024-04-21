a0dbdee960631b33386782e1480a0389c5c8f4a1
ismael <ismaelreyplata@gmail.com>
2024-04-21 13:42:17 +0200

    con bootstrap instalado

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,10 @@
+import { bootstrapApplication } from '@angular/platform-browser';
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+
+
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes)]
