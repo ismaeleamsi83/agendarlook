919de18be2c3b4dde50d5c190ad192ae63e40bd4
ismael <ismaelreyplata@gmail.com>
2024-04-24 13:01:28 +0200

    guardo el formulario

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -3,7 +3,7 @@ import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import { LoginComponent } from '../login/login.component';
 import { config } from 'rxjs';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -13,5 +13,26 @@ import { ReactiveFormsModule } from '@angular/forms';
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+
+  registerForm: FormGroup;
+  
+
+  constructor() {
+    this.registerForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      lastname: new FormControl('', [Validators.required]),
+    });
+  }
+
+  submitForm() {
+    if (this.registerForm.valid){
+      console.log(this.registerForm.value);
+    }else{
+      console.log("no valido");
+    }
+    
+  }
+
+  
 
 }
