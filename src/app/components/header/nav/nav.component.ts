feee5ce99641a626a1f570bf9edc7ec5461f4421
ismael <ismaelreyplata@gmail.com>
2024-04-27 12:18:11 +0200

    configuration diseño ok nav retocado

--- src/app/components/header/nav/nav.component.ts
+++ src/app/components/header/nav/nav.component.ts
@@ -1,9 +1,10 @@
+import { RouterLink } from '@angular/router';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss'
 })
